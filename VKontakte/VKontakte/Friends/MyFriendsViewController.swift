789e6c11827889f195f10369dcacaa4e66249112
k2el <kovtunmo@me.com>
2020-08-10 15:13:42 +0500

    add task9

--- VKontakte/VKontakte/Friends/MyFriendsViewController.swift
+++ VKontakte/VKontakte/Friends/MyFriendsViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 
 
 class MyFriendsViewController: UITableViewController, UISearchBarDelegate {
+
     
     var sections: [String] = []
     var newMyFriends: [[User]] = []
---
+++ VKontakte/VKontakte/Test.swift
@@ -0,0 +1,11 @@
+//
+//  Test.swift
+//  VKontakte
+//
+//  Created by Анна Ковтун on 31.07.2020.
+//  Copyright © 2020 artGK. All rights reserved.
+//
+
+import UIKit
+
+
